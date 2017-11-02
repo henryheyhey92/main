314162f29da6781e73a58221fffdca192e41d6fd
henrylaifeng <e0032134@u.nus.edu>
2017-11-03 06:25:50 +0800

    edit travis week11.1

--- src/main/java/seedu/address/ConfirmBox.java
+++ src/main/java/seedu/address/ConfirmBox.java
@@ -1,18 +1,24 @@
 package seedu.address;
 
+import javafx.geometry.Pos;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.geometry.Pos;
+import javafx.scene.Scene;
 
+
+/**
+ * To confirm that the user wanted to exit the program
+ * Avoid accidental shutdown of the addressbook
+ */
 //@@author henryheyhey92
 public class ConfirmBox {
     private static boolean answer;
 
     public static boolean display(String title, String message) {
+        //initialise the comfirm box window
         Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
@@ -34,7 +40,7 @@ public class ConfirmBox {
             answer = false;
             window.close();
         });
-
+        // add VBox
         VBox layout = new VBox(10);
 
         //Add buttons
