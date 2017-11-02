78e28da7d2ae16a355506f66d5de6b573a9e2ccc
nj <e0053204@u.nus.edu>
2017-11-03 05:01:50 +0800

    Replace filter non-alphanumeric with filter [ ] for each tag.

--- src/main/java/seedu/address/logic/commands/ListTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTagsCommand.java
@@ -13,32 +13,20 @@ public class ListTagsCommand extends Command {
     public static final String COMMAND_WORD = "listTags";
     public static final String MESSAGE_SUCCESS = "List all tags:\n%1$s";
     public static final String MESSAGE_NO_TAGS = "There are no tags available.";
-    /*
-    private List<String> getTagList (){
-        List<String> listOfTags = new ArrayList<>();
 
-        ObservableList<ReadOnlyPerson> listOfPersons = model.getFilteredPersonList();
-
-        listOfPersons.forEach(persons-> persons.getTags().forEach(tags -> listOfTags.add(tags.toString()
-                .replaceAll("[^a-zA-Z]", "")))); //removes all non-letters
-
-        //returns list with duplicates removed and sorted
-        listOfTags.stream().distinct().sorted().collect(Collectors.toList());
-        return listOfTags;
-    }*/
     @Override
     public CommandResult execute() {
         List<Tag> listOfTags = model.getTagsList();
         List<String> tagsString = new ArrayList<>();
 
-        //removes all [] and only show default alphanumeric regex for tags
-        listOfTags.forEach(tag -> tagsString.add(tag.toString()));//.replaceAll("[a-zA-Z]", "")));
+        //removes all [] from tags
+        listOfTags.forEach(tag -> tagsString.add(tag.toString().replaceAll("[\\[\\]]", "")));
 
         if (listOfTags.isEmpty()) {
             return new CommandResult(MESSAGE_NO_TAGS);
         }
 
         System.out.println(listOfTags);
-        return new CommandResult(String.format(MESSAGE_SUCCESS, tagsString));//String.join("\n", tagsString)));
+        return new CommandResult(String.format(MESSAGE_SUCCESS, String.join("\n", tagsString)));
     }
 }
