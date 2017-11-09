5885d8309977420e9460794d691d0e3fc32cc06c
nj <e0053204@u.nus.edu>
2017-11-10 06:25:31 +0800

    Change naming convention and update userguide for listTag command

--- src/main/java/seedu/address/logic/commands/ListTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTagsCommand.java
@@ -11,14 +11,14 @@ import java.util.List;
  */
 public class ListTagsCommand extends Command {
 
-    public static final String CHOICE = "f";
+    public static final String FILTERED = "f";
 
     public static final String COMMAND_WORD = "listTags";
     public static final String MESSAGE_SUCCESS = "List all tags:\n%1$s";
     public static final String MESSAGE_NO_TAGS = "There are no tags available.";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds tags according to choice.\n"
             + "1. To list all tags available in AddressBook: " + COMMAND_WORD + "\n"
-            + "2. To list all tags of persons currently shown below: " + COMMAND_WORD + " " + CHOICE + "\n";
+            + "2. To list all tags of persons currently shown below: " + COMMAND_WORD + " " + FILTERED + "\n";
 
     private String option;
 
@@ -33,7 +33,7 @@ public class ListTagsCommand extends Command {
         requireNonNull(model);
 
         switch (option) {
-        case CHOICE:
+        case FILTERED:
             listOfTags = model.getTagsListAsString(model.getFilteredTagsList());
             break;
         default:
