1b59ff8f665235ae446e352dde68ae1ab013261c
henrylaifeng <e0032134@u.nus.edu>
2017-11-09 20:23:56 +0800

    edit on import statement.

--- src/main/java/seedu/address/ConfirmBox.java
+++ src/main/java/seedu/address/ConfirmBox.java
@@ -3,9 +3,9 @@ package seedu.address;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
+import javafx.scene.layout.VBox;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
