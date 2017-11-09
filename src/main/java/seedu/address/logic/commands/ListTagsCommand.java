3fe8bc3bd204bd193fb9ebc524d6af7358922ef9
nj <e0053204@u.nus.edu>
2017-11-10 02:43:08 +0800

    Improving listTags: Add parser for ListTags that takes in either listTags (for normal list) or listTags f (for filtered list)

--- src/main/java/seedu/address/logic/commands/ListTagsCommand.java
+++ src/main/java/seedu/address/logic/commands/ListTagsCommand.java
@@ -11,10 +11,16 @@ import seedu.address.model.tag.Tag;
  */
 public class ListTagsCommand extends Command {
 
+    public static final String CHOICE = "f";
+
     public static final String COMMAND_WORD = "listTags";
     public static final String MESSAGE_SUCCESS = "List all tags:\n%1$s";
     public static final String MESSAGE_NO_TAGS = "There are no tags available.";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds tags according to choice.\n"
+            + "1. To list all tags available in AddressBook: " + COMMAND_WORD + "\n"
+            + "2. To list all tags of persons currently shown below: " + COMMAND_WORD + " " + CHOICE + "\n";
 
+    public ListTagsCommand(String option){}
     @Override
     public CommandResult execute() {
         List<Tag> listOfTags = model.getTagsList();
