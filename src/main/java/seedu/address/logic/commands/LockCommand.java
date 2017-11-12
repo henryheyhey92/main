feb5ce5a5bdc6362ff78d3af691a81eefc64fa8b
henrylaifeng <e0032134@u.nus.edu>
2017-11-12 17:53:20 +0800

    fix check style on adoc and lockcommand

--- src/main/java/seedu/address/logic/commands/LockCommand.java
+++ src/main/java/seedu/address/logic/commands/LockCommand.java
@@ -11,31 +11,18 @@ import seedu.address.storage.Storage;
  */
 public class LockCommand extends UndoableCommand {
 
-    private static Stage window = new Stage();
     public static final String COMMAND_WORD = "lock";
     public static final String MESSAGE_SUCCESS = "Address book is unlock!";
-    public static final String MESSAGE_FAIL = "Address book is lock";
-    public static LoginBox lock;
-    //public static boolean answer;
     protected Storage storage;
     private boolean name = false;
 
-    //    public LockCommand(){
-////        lock = new LoginBox();
-////        lock.display("AddressBook Login", 1);
-//
-//    }
     public LockCommand(boolean name){
         this.name = name;
     }
-    public boolean LockCommandget(){
-        return this.name;
-    }
-
 
     @Override
     public CommandResult executeUndoableCommand() {
-            return new CommandResult(MESSAGE_SUCCESS);
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 
 }
