fc5f4fbbe6c5db6c4324e21fbfccf37ad0e60656
firedude123 <benjaminasj@hotmail.com>
2017-11-12 18:43:52 +0800

    Travis fix 6

--- src/test/java/systemtests/AddressBookSystemTest.java
+++ src/test/java/systemtests/AddressBookSystemTest.java
@@ -48,7 +48,6 @@ public abstract class AddressBookSystemTest {
     private static final List<String> COMMAND_BOX_DEFAULT_STYLE = Arrays.asList("text-input", "text-field");
     private static final List<String> COMMAND_BOX_ERROR_STYLE =
             Arrays.asList("text-input", "text-field", CommandBox.ERROR_STYLE_CLASS);
-    
     @ClassRule
     public static ClockRule clockRule = new ClockRule();
     private MainWindowHandle mainWindowHandle;
